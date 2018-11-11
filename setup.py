c2f3ca559658721125d3a50737256aabada55b9f
Raphael Guntersweiler <raphael@rgunti.ch>
2018-11-11 22:05:47 +0100

    Added long description

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ Licensed under MIT
 
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name='carpi-redisdatabus',
       version='0.1',
       description='Redis Data Bus (inspired by CAN-BUS, developed for CarPi)',
+      long_description=long_description,
       url='https://github.com/rGunti/CarPi-RedisDataBus',
       keywords='carpi redis data bus',
       classifiers=[
